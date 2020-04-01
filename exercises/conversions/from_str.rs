e3f93850027f448330eee9ea30a95e3bb9a61ac6
Jiayu Liu <jiayu.liu@airbnb.com>
2020-04-01 22:01:36 +0800

    done

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -10,7 +10,6 @@ struct Person {
     age: usize,
 }
 
-// I AM NOT DONE
 // Steps:
 // 1. If the length of the provided string is 0, then return an error
 // 2. Split the given string on the commas present in it
@@ -21,6 +20,22 @@ struct Person {
 impl FromStr for Person {
     type Err = String;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err("".into())
+        }
+        let tokens: Vec<&str> = s.split(',').collect();
+        if tokens.len() != 2 {
+            return Err("".into())
+        }
+        let name = tokens[0].to_string();
+        if let Ok(age) = tokens[1].parse::<usize>() {
+            Ok(Person {
+                name,
+                age
+            })
+        } else {
+            Err("".into())
+        }
     }
 }
 
